3623cfaa7d61248e217d75ebd8c4ca6649229e15
raisiali2 <aliraisi807@gmail.com>
2024-02-23 07:38:50 +0100

    variables

---
+++ variable.py
@@ -0,0 +1,11 @@
+print( len( input("what is your name: ") ) )
+
+# now i want to rewrite the code with variable that we can to refer and access it anytime
+
+
+name = "ali"
+print(name)
+
+name = "ahmad"
+print(name)
+
