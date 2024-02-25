281ce8b8a48e0ae0801ff8b4c9a8f04dfbeb5008
raisiali2 <aliraisi807@gmail.com>
2024-02-25 12:40:57 +0100

    Add variable

--- variable.py
+++ variable.py
@@ -1,4 +1,4 @@
-print( len( input("what is your name: ") ) )
+#print( len( input("what is your name: ") ) )
 
 # now i want to rewrite the code with variable that we can to refer and access it anytime
 
@@ -9,3 +9,9 @@ print(name)
 name = "ahmad"
 print(name)
 
+# this variable each of them taking different value and it makes coding very easy
+
+# if we write the previous program using variable we can write it more explicit and readable way
+name = input("enter your name?")
+length = len(name)
+print(length)
